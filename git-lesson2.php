f379c27c1b7860a49a3a3abb9cc999e280705ab9
Luu Xuan Dat <luuxuandat197@gmail.com>
2020-11-27 19:27:52 +0700

    tao file git-lesson2.php

---
+++ git-lesson2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @Author Lưu xuân Đạt -
+ * @Github luuxuandat197@gmail.com
+ * @create 11/27/20 7:19 PM
+ */
+
+//----------------------- Hoc Git - Phan 2
+echo '<pre>';
+?>
+
+---------- Cac buoc lam viec voi git ---------
+-- fork
+-- clone
+-- checkout -b lesson2
+-- viet code
+-- git status
+-- git add .
+-- git commit -m "mo ta commit"
+-- git push <origin_name> <branch_name>
+-- git log
+-- git log --oneline -5

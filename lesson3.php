ed37fc282783dda3a5677f71806a2ed1f4365c5b
chuviethoang123 <hoangneymar000@gmail.com>
2020-11-30 19:37:49 +0700

    chuviethoang

--- lesson3.php
+++ lesson3.php
@@ -2,15 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=ee, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>namename</h1>
     <?php
-    chuviethoang
+    ChU VIET HOANG
     php2008
     ?>
 </body>
 </html>
-
